e4c8c116f778338aaca79a640572ba142373db25
Pau Hermosilla Del Rio <pauhermosillad@gmail.com>
2019-12-12 13:42:17 +0100

    fix(#4): script buid.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,4 @@ cp -r ${SRCPATH} ${DISTPATH}
 mkdir ${CSSpATH}
 node-sass ${DISTPATH}/assets/scss/main.scss ${CSSpATH}/styles.css
 rm -rf ${DISTPATH}/assets/scss
-rm -rf ${DISTPATH}/assets/components
+rm -rf ${DISTPATH}/assets/components/*/*.scss
