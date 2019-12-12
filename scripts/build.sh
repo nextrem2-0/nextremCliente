a699760ae2967e62ecf55a3702e5b54321afc540
Pau Hermosilla Del Rio <pauhermosillad@gmail.com>
2019-12-12 09:53:38 +0100

    feat(#3): Html y css estatico

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,3 +7,4 @@ cp -r ${SRCPATH} ${DISTPATH}
 mkdir ${CSSpATH}
 node-sass ${DISTPATH}/assets/scss/main.scss ${CSSpATH}/styles.css
 rm -rf ${DISTPATH}/assets/scss
+rm -rf ${DISTPATH}/assets/components
