49ee86d53f17d920cc66decc01f904b2d3122a5b
dani <danielalbero98@gmail.com>
2020-01-07 12:18:38 +0100

    feat(#16): Loader

--- src/js/main.js
+++ src/js/main.js
@@ -7,55 +7,87 @@ let imgSlider;
 let slider;
 let sports;
 let events;
+ 
+$(".l-page").hide();
 
 window.onload = function () {
-    categories= new Array();
-    listaCards=new Array();
-    listaPins=new Array();
-    imgSlider=new Array();
-    sports=new Array();
-    events=["bbbbb","bbbbb","bbbbb","bbbbb","bbbbb"];
-
-    $.ajax({
-        url:"http://localhost/nextrem/api/public/api/categoria",
-        success: function(dataResult){
-            for (let key of dataResult) {
-                categories.push(key.nombre);
-            }
-
-            let footer = new Footer(categories);
-            $("#footer").append(footer.draw());
-            $.ajax({
-                url:"http://localhost/nextrem/api/public/api/getDeportes",
-                success: function(dataResult){
-                    for (let key of dataResult) {
-                        sports.push(key.nombre);
-                    }
-                    menu=new Menu(categories,sports,events);
-                    $("#menu").append(menu.draw());
-                    $(".c-submenu").hide();
-                }
-            });
-        }
-    });
-
-    cargarInicio();
-}; 
+    setTimeout(function () {
+        finPaginaCarga();
+    }, 1200);
+};
 
 
 /* --------- FUNCIONES --------- */
+categories = new Array();
+listaCards = new Array();
+listaPins = new Array();
+imgSlider = new Array();
+sports = new Array();
+events = ["bbbbb", "bbbbb", "bbbbb", "bbbbb", "bbbbb"];
+
+$.ajax({
+    url: "http://localhost/nextrem/api/public/api/categoria",
+    success: function (dataResult) {
+        for (let key of dataResult) {
+            categories.push(key.nombre);
+        }
+
+        let footer = new Footer(categories);
+        $("#footer").append(footer.draw());
+        $.ajax({
+            url: "http://localhost/nextrem/api/public/api/getDeportes",
+            success: function (dataResult) {
+                for (let key of dataResult) {
+                    sports.push(key.nombre);
+                }
+                menu = new Menu(categories, sports, events);
+                $("#menu").append(menu.draw());
+                $(".c-submenu").hide();
+            }
+        });
+    }
+});
+
+cargarInicio();
+
+/* function paginaCarga() {
+    $(".l-page").hide();
+    let $page = $("<div>", { "class": "l-carga" }).append(
+        $("<div>", {
+            "class": "c-carga"
+        })
+    )
+    $("body").prepend($page);
+    $(".c-carga").append([
+        $('<div>', { "class": "l-carga__item l-carga__item--logo" }).append(
+            $('<img>', { "class": "c-carga__img", "src": "assets/img/nextrem.png" })
+        ),
+        $('<div>', { "class": "l-carga__item l-carga__item--gif" }).append(
+            $('<img>', { "class": "c-carga__gif", "src": "assets/img/load.gif" })
+        )
+    ]);
+} */
+
+function finPaginaCarga() { 
+    $(".c-load").hide("slow");
+    setTimeout(function () {
+        $(".l-page").show();
+    }, 500); 
+    
+}
+
 function deleteContenido() {
-    $( "#content" ).empty();
+    $("#content").empty();
 }
 
 function cargarInicio() {
     deleteContenido();
-    
+
     $.ajax({
-        url:"http://localhost/nextrem/api/public/api/getImages",
-        success: function(dataResult){
+        url: "http://localhost/nextrem/api/public/api/getImages",
+        success: function (dataResult) {
             for (let key of dataResult) {
-                
+
                 imgSlider.push(key);
             }
             cargarSlider();
@@ -67,29 +99,30 @@ function cargarInicio() {
     cargarPins();
     cargarCards();
 
-    $layout=$("<div>",{
-        "class":"l-columns--1-column"});
-    
-    $item1=$("<div>",{
-        "class":"l-columns__item" 
-    });
-    
-
-    $item2=$("<div>",{
-        "class":"l-columns__item"
+    $layout = $("<div>", {
+        "class": "l-columns--1-column"
     });
 
-    $item3=$("<div>",{
-        "class":"l-columns__item"
+    $item1 = $("<div>", {
+        "class": "l-columns__item"
     });
-    $item3.append($("<div>",{
-        "class":"c-section__image"
+
+
+    $item2 = $("<div>", {
+        "class": "l-columns__item"
+    });
+
+    $item3 = $("<div>", {
+        "class": "l-columns__item"
+    });
+    $item3.append($("<div>", {
+        "class": "c-section__image"
     }));
 
-    $item4=$("<div>",{
-        "class":"l-columns__item"
+    $item4 = $("<div>", {
+        "class": "l-columns__item"
     });
-    s2=new Section("l-columns",listaCards,"RECOMMENDED","l-columns--3-columns");
+    s2 = new Section("l-columns", listaCards, "RECOMMENDED", "l-columns--3-columns");
     $item4.append(s2.draw());
 
     $layout.append($item1);
@@ -97,42 +130,42 @@ function cargarInicio() {
     $layout.append($item3);
     $layout.append($item4);
 
-    
+
 
     $("#content").append($layout);
 
     
 }
 
-function cargarPins(){
+function cargarPins() {
     $.ajax({
-        url:"http://localhost/nextrem/api/public/api/textInicio",
-        success: function(dataResult){
-                
+        url: "http://localhost/nextrem/api/public/api/textInicio",
+        success: function (dataResult) {
+
             for (let key in dataResult) {
-                
-                listaPins.push(new InformationPin(dataResult[key].imagen,dataResult[key].titulo,dataResult[key].texto));
-                
+
+                listaPins.push(new InformationPin(dataResult[key].imagen, dataResult[key].titulo, dataResult[key].texto));
+
             }
-            s1=new Section("l-horizontal",listaPins,"WHAT NEXTREM IS?");
+            s1 = new Section("l-horizontal", listaPins, "WHAT NEXTREM IS?");
             $item2.append(s1.draw());
         },
-        error: function(error){
+        error: function (error) {
             console.log(error);
-            
+
         }
     });
 }
 
-function cargarCards(){
+function cargarCards() {
     var descrip = "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.";
     var iconos = ['fa fa-mountain', 'fa fa-users', 'fa fa-hiking'];
-    listaCards.push(new Card("evento1.jpg","Liga de escuelas", "Escalada", descrip, iconos, 3, 'escalada'));
-    listaCards.push(new Card("evento2.jpg","Torneo de Surf", "Surf", descrip, iconos, 2, 'surf'));
-    listaCards.push(new Card("evento3.jpg","Clases de ski", "Snow", descrip, iconos, 3, 'nieve'));
+    listaCards.push(new Card("evento1.jpg", "Liga de escuelas", "Escalada", descrip, iconos, 3, 'escalada'));
+    listaCards.push(new Card("evento2.jpg", "Torneo de Surf", "Surf", descrip, iconos, 2, 'surf'));
+    listaCards.push(new Card("evento3.jpg", "Clases de ski", "Snow", descrip, iconos, 3, 'nieve'));
 
 }
 
-function cargarSlider(){
-    slider=new Slider(imgSlider);
+function cargarSlider() {
+    slider = new Slider(imgSlider);
 }
